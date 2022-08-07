144f530dc9e5fac6c3ea1dbdd5726ebf9f09ed3d
ShootGan <nawelpalepka@gmail.com>
2022-08-07 15:41:09 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ cd "${GITHUB_WORKSPACE}" || exit
 VERSION_FILE="${FILE:-package.json}"
 EXISTING_VERSIONS=`git tag`
 NEW_VERSION="v`grep version $VERSION_FILE | sed 's/.*"version": "\(.*\)".*/\1/'`"
-
+if 
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
@@ -22,8 +22,9 @@ fi
 echo "Creating tag"
 git tag -a "${NEW_VERSION}" -m "realese ${NEW_VERSION}"
 echo "pushing tag to repository"
-if [ -n "${INPUT_GITHUB_TOKEN}" ]; then
+if [ -z "${GITHUB_TOKEN}" ]; then
   git remote set-url origin "https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+  echo "using token"
 fi
 git push origin --tags
 echo "tag created"
