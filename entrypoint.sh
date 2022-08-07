d432624491c0edc379290e76322928d026987928
ShootGan <nawelpalepka@gmail.com>
2022-08-07 13:47:42 +0200

    first commit

---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+# Apply hotfix for 'fatal: unsafe repository' 
+git config --global --add safe.directory "${GITHUB_WORKSPACE}"
+cd "${GITHUB_WORKSPACE}" || exit
+
+#Set variables
+VERSION_FILE="${FILE}:-package.json"
+EXISTING_VERSIONS=`git tag`
+NEW_VERSION="v`grep version $VERSION_FILE | sed 's/.*"version": "\(.*\)".*/\1/'`"
+
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+
+echo "looking for version"
+echo $NEW_VERSION 
+if [[ $EXISTING_VERSIONS == *$NEW_VERSION* ]]; then
+  echo "tag already exists"
+  exitCode 1 
+fi
+echo "Creating tag"
+git tag -a "${NEW_VERSION}" -m "realese ${NEW_VERSION}"
+echo "pusing tag to repository"
+git push origin --tags
+echo "tag created"
