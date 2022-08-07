05a21a4ad2734dda738507151bdc58b830ea2787
ShootGan <nawelpalepka@gmail.com>
2022-08-07 14:36:20 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,19 +8,19 @@ cd "${GITHUB_WORKSPACE}" || exit
 #Set variables
 VERSION_FILE="${FILE}:-package.json"
 EXISTING_VERSIONS=`git tag`
-NEW_VERSION="v`grep version $VERSION_FILE | sed 's/.*"version": "\(.*\)".*/\1/'`"
-
+NEW_VERSION=`grep version $VERSION_FILE | sed 's/.*"version": "\(.*\)".*/\1/'`
+VERSION="v${NEW_VERSION}"
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 ls 
 echo "looking for version"
-echo $NEW_VERSION 
-if [[ $EXISTING_VERSIONS == *$NEW_VERSION* ]]; then
+echo $VERSION 
+if [[ $EXISTING_VERSIONS == *$VERSION* ]]; then
   echo "tag already exists"
   exit 1 
 fi
 echo "Creating tag"
-git tag -a "${NEW_VERSION}" -m "realese ${NEW_VERSION}"
+git tag -a "${VERSION}" -m "realese ${VERSION}"
 echo "pusing tag to repository"
 git push origin --tags
 echo "tag created"
